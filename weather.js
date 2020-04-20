86a20b57dcc97d8c7899c99e84a1a0e65bd930b7
Cherish <62115637+CherisMoor@users.noreply.github.com>
2020-04-20 08:58:02 -0400

    new updates

--- weather.js
+++ weather.js
@@ -1,19 +1,21 @@
 
 
 //var input = document.querySelector('.userInput');
+
 var main = document.querySelector('#cityName');
 var temperature = document.querySelector('.temperature');
 var weather = document.querySelector('.description');
 var button = document.querySelector('.submit');
 var zip = document.querySelector('.zipcode');
 var time = document.querySelector('.time');
+var date = document.querySelector('.date');
+//var icon = document.querySelector('.icon');
+
 
 function degreesKtoF(num) {
     return Math.floor( (num - 273) * (9/5) + 32)
-}
-function degreesKtoC(num) {
-    return num - 273;
-}
+};
+
 
 button.addEventListener('click', function(){
 
@@ -32,19 +34,51 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${input}&appid=1fe334
   var temperatureValue = degreesKtoF(data['main']['temp']);
   var nameValue = data['name'];
   var weatherValue = data['weather'][0]['description'];
-  var timeValue = [moment().format("h:mm a")];
+//  var timeValue = ['timezone']
+//  var sec = 1587331994;
+//  var date = new Date(sec * 1000);
+  var lon = data.coord.lon;
+  var lat = data.coord.lat;
+//  var date = moment().utcOffset(timeValue / 60).format("h:mm a z");
+//  console.log(date, timestr);
 
+//  $(".time").html(timestr);
+//  $(".date").html(date);
+$.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=1fe3341f2f29bd0cda3bcdc84b8acdcc&units=imperial`, ((time)=>{
+
+//grabs the timezone of the lon and lat and formats it using the moment library then displays on the browser
+  let datetime= moment().tz(time.timezone).format('dddd MMMM Do YYYY, h:mm a z');
+$('#displayMoment').text(datetime);
+}))
+
+
+  function getTime(time) {
+  //converts time based on utc offset, which api provides
+  return moment().utcOffset(time / 60).format("h:mm a z")
+}
+  var timeValue = getTime();
+
+  //var utcTime = getTime();
+  //4-hours in milliseconds..  var actTime = 1.44e+7;
+  //var timeValue = utcTime - actTime;
+//  var dateValue = $(".date").html(date);
+
+  //var weatherIcon = data['weather'][0].icon;
+
+
+  //icon.innerHTML = weatherIcon;
   main.innerHTML = nameValue;
   weather.innerHTML = "Weather: " + weatherValue;
-  temperature.innerHTML = "Temperature: " + temperatureValue + " F";
+  temperature.innerHTML = "Temperature: " + temperatureValue + " °F";
   zip.innerHTML = "Zip Code: " + zipcodeValue;
   time.innerHTML = "Time: " + timeValue;
+//  date.innerHTML = "Date: " + dateValue;
   input.value ="";
 
 
-})
-})
+});
+});
 
-//.catch(err => alert("Wrong zip code!"));
-
+.catch(err => alert("Wrong zip code!"));
+//$(".icon").html("<img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png' alt='Icon depicting current weather.'>");
 
